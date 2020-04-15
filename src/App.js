be0fca474212f7ce30a3120d5cdc5f5a9c27e677
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-04-16 09:16:15 +1000

    feat: add navbar & routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Auth, Hub } from "aws-amplify";
-import { Authenticator, Greetings, AmplifyTheme } from "aws-amplify-react";
+import { Authenticator, AmplifyTheme } from "aws-amplify-react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import MarketPage from "./pages/MarketPage";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 class App extends React.Component {
@@ -39,7 +44,21 @@ class App extends React.Component {
     return !user ? (
       <Authenticator theme={theme} hideDefault={true} />
     ) : (
-      <div>App</div>
+      <Router>
+        <>
+          <Navbar />
+          <div className="app-container">
+            <Route exact path="/" component={HomePage} />
+            <Route path="/profile" component={ProfilePage} />
+            <Route
+              path="/markets/:marketId"
+              component={({ match }) => (
+                <MarketPage marketId={match.params.marketId} />
+              )}
+            />
+          </div>
+        </>
+      </Router>
     );
   }
 }
