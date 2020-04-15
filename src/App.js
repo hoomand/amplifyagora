28040c64a0d416fff2b1b7083a80a3eb5601621d
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-04-16 08:47:42 +1000

    ref: replace withAuthenticator HOC with Authenticator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from "react";
-import { withAuthenticator, AmplifyTheme } from "aws-amplify-react";
+import { Auth, Hub } from "aws-amplify";
+import { Authenticator, Greetings, AmplifyTheme } from "aws-amplify-react";
 import "./App.css";
 
 class App extends React.Component {
-  state = {};
+  state = { user: null };
+
+  componentDidMount() {
+    this.getUserData();
+  }
+
+  getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser();
+    user ? this.setState({ user }) : this.setState({ user: null });
+    Hub.listen("auth", this, "onHubCapsule");
+  };
+
+  onHubCapsule = capsule => {
+    switch (capsule) {
+      case "signIn":
+        console.log("user signed in!");
+        this.getUserData();
+        break;
+      case "signUp":
+        console.log("signed up");
+        break;
+      case "signOut":
+        console.log("signed out");
+        this.setState({ user: null });
+        break;
+      default:
+        return;
+    }
+  };
 
   render() {
-    return <div>App</div>;
+    const { user } = this.state;
+    return !user ? (
+      <Authenticator theme={theme} hideDefault={true} />
+    ) : (
+      <div>App</div>
+    );
   }
 }
 
@@ -27,4 +61,4 @@ const theme = {
   }
 };
 
-export default withAuthenticator(App, true, [], null, theme);
+export default App;
